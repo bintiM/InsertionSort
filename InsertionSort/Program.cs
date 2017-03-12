d2a594d4c0ca74e8b0ffa8fb1aa96b4c0a73f013
bintiM <marc.bintinger@yahoo.com>
2017-03-12 08:57:41 +0100

    Kommentare

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -10,9 +10,14 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("unsortiertes Array");
             int[] arr = { 5, 9, 1, 4, 6, 7, 3, 2, 8 };
 
+            foreach(int i in arr)
+                Console.Write(i + " ");
+
+
+            Console.WriteLine("\nsortiertes Array");
             int[] sorted = insSort(arr);
 
             for (int i = 0; i < sorted.Length; i++)
@@ -25,17 +30,17 @@ namespace InsertionSort
         static int[] insSort(int[] array)
         {
  
-            for (int i = 1; i < array.Length; i++)
+            for (int i = 1; i < array.Length; i++) //ganzes Array einmal durch
             {
-                int j = i;
+                int j = i; // aktuelle Startposition für innere Schleife setzen
 
-                while ((j > 0) && (array[j] < array[j - 1]))
+                while ((j > 0) && (array[j] < array[j - 1])) // wenn der rechte Wert kleiner ist als der linke
                 {
-                    int temp = array[j -1];
-                    array[j - 1] = array[j];
-                    array[j] = temp;
+                    int temp = array[j - 1]; // linken Wert merken
+                    array[j - 1] = array[j]; // rechten Wert nach links setzen
+                    array[j] = temp; // gemerkte Wert nach rechts setzen
 
-                    j--;
+                    j--; // Iterator verkleinern, d.h. die innere Schleife geht nach links bis zum Anfang
                 }
             }
             return array;
